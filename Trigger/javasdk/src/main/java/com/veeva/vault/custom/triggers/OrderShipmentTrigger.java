6fd305d6031c8bad242f2925abfdf7ab1c1cd299
Srinija <snalluri32@gatech.edu>
2025-07-19 17:13:30 -0400

    Update code

--- Trigger/javasdk/src/main/java/com/veeva/vault/custom/triggers/OrderShipmentTrigger.java
+++ Trigger/javasdk/src/main/java/com/veeva/vault/custom/triggers/OrderShipmentTrigger.java
@@ -7,34 +7,43 @@ import com.veeva.vault.sdk.api.data.RecordTriggerContext;
 import com.veeva.vault.sdk.api.data.RecordChange;
 import com.veeva.vault.sdk.api.core.ValueType;
 import com.veeva.vault.sdk.api.data.Record;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import java.time.LocalDateTime;
 import com.veeva.vault.sdk.api.data.*;
 
+import java.util.List;
+import java.time.ZonedDateTime;
 
-@RecordTriggerInfo(object = "order__c", name = "create_shipment_trigger", events = RecordEvent.AFTER_INSERT)
+
+@RecordTriggerInfo(object = "order__c", events = RecordEvent.AFTER_INSERT)
 public class OrderShipmentTrigger implements RecordTrigger {
 
     public void execute(RecordTriggerContext recordTriggerContext) {
 
         RecordService recordService = ServiceLocator.locate(RecordService.class);
-        List<Record> shipments = new ArrayList<>();
+        List<Record> shipments = VaultCollections.asList();
 
-        for (RecordChange change : recordTriggerContext.getRecordChanges()) {
-            String orderId = change.getNew().getValue("id", ValueType.STRING);
+        for (RecordChange newOrder : recordTriggerContext.getRecordChanges()) {
+            String orderId = newOrder.getNew().getValue("id", ValueType.STRING);
+            String orderName = newOrder.getNew().getValue("name__v", ValueType.STRING);
+            String address = "1500 Broadway Ave New York, NY";
 
             Record shipment = recordService.newRecord("shipment__c");
-            shipment.setValue("name__v", "Shipment for Order " + orderId);
+            shipment.setValue("name__v", "Shipment For " + orderName);
             shipment.setValue("order__c", orderId);
-            shipment.setValue("date__c", LocalDateTime.now().plusDays(1));
-            shipment.setValue("shipment_tracking_number__c", UUID.randomUUID().toString());
+            shipment.setValue("date__c", ZonedDateTime.now().plusDays(1));
+            String trackingNumber = "trk" + "-" + orderId;
+            shipment.setValue("address__c", address);
+            shipment.setValue("shipment_tracking_number__c", trackingNumber);
 
             shipments.add(shipment);
         }
-        recordService.batchSaveRecords(shipments).execute();
+        if (!shipments.isEmpty()) {
+            RecordBatchSaveRequest recordSaveRequest = recordService.newRecordBatchSaveRequestBuilder().withRecords(shipments).build();
+            recordService.batchSaveRecords(recordSaveRequest)
+                    .onErrors(errors -> {
+                        throw new RollbackException("ORDER_CREATE_FAIL", "Failed to create Order.");
+                    })
+                    .execute();
+        }
 
     }
 }
